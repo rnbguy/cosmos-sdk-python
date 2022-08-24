1c83547a4d0e5e8d6fe04431410993312d5c29f9
Ranadeep Biswas <mail@rnbguy.at>
2022-08-24 19:49:20 +0200

    example

---
+++ examples/query_all_balances.py
@@ -0,0 +1,27 @@
+import json
+
+import grpc
+from google.protobuf.json_format import MessageToDict, ParseDict
+
+from cosmos.bank.v1beta1.query_pb2 import QueryAllBalancesRequest
+from cosmos.bank.v1beta1.query_pb2_grpc import QueryStub
+
+endpoint = ":9090"
+
+# Binance validator address balance
+json_payload = {"address": "cosmos156gqf9837u7d4c4678yt3rl4ls9c5vuuxyhkw6"}
+pb_payload = ParseDict(json_payload, QueryAllBalancesRequest())
+
+
+with grpc.insecure_channel(endpoint) as channel:
+    stub = QueryStub(channel)
+    balance_data = stub.AllBalances(pb_payload)
+
+# # or, using experimental api
+# from cosmos.bank.v1beta1.query_pb2_grpc import Query
+# balance_data = Query.AllBalances(
+#     pb_payload, endpoint, insecure=True
+# )
+
+balance_json = MessageToDict(balance_data)
+print(json.dumps(balance_json, indent=2))
